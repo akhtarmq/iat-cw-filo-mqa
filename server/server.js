8887b9ea394d67e3e7e35a2c68a836f117a23367
akhtarmq <akhtarmq@aston.ac.uk>
2020-05-03 14:57:26 +0100

    Complete Commit. V0.02

--- server/server.js
+++ server/server.js
@@ -26,6 +26,8 @@ MongoClient.connect(connectionStringRemote, { useUnifiedTopology: true })
     // ========================
     app.set('view engine', 'ejs')
     app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use(express.static('public'))
 
     app.get('/', (req, res) => {
       db.collection('quotes').find().toArray()
@@ -33,7 +35,7 @@ MongoClient.connect(connectionStringRemote, { useUnifiedTopology: true })
           res.render('index.ejs', { quotes: results })
         })
         .catch(/* ... */)
-    })
+    })  
     
     app.post('/quotes', (req, res) => {
       quotesCollection.insertOne(req.body)
@@ -43,6 +45,36 @@ MongoClient.connect(connectionStringRemote, { useUnifiedTopology: true })
         .catch(error => console.error(error))
     })
 
+    app.put('/quotes', (req, res) => {
+      quotesCollection.findOneAndUpdate(
+        { name: 'Yoda' },
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+        .then(result => res.json('Success'))
+        .catch(error => console.error(error))
+    })
+
+    app.delete('/quotes', (req, res) => {
+      quotesCollection.deleteOne(
+        { name: req.body.name }
+      )
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json('No quote to delete')
+          }
+          res.json('Deleted Darth Vadar\'s quote')
+        })
+        .catch(error => console.error(error))
+    })
+    
     app.listen(3000, function() {
       console.log('listening on 3000')
     }
