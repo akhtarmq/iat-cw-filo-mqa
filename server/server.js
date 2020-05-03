f1b083e8c666a8e57a47f0bb6856db4d3b2ffcb2
akhtarmq <akhtarmq@aston.ac.uk>
2020-05-03 03:23:40 +0100

    Working Version V0.01
    Current Working Version

--- server/server.js
+++ server/server.js
@@ -1,21 +1,51 @@
 // server.js
-console.log('May Node be with you')
-
+const express = require('express')
+const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
-const dbName = 'testdb'
-const connectionString = 'mongodb://localhost:27017/' + dbName
+const app = express()
 
-MongoClient.connect(connectionString, {
-  useUnifiedTopology: true
-}).then(client => {
+const dbName = 'testdb'
+const connectionStringLocal = 'mongodb://localhost:27017/' + dbName
+const connectionStringRemote = "mongodb+srv://admin:admin2020@cluster0-bjw97.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+MongoClient.connect(connectionStringRemote, { useUnifiedTopology: true })
+  .then(client => {
     console.log('Connected to Database')
-	const db = client.db(dbName)
-	const testCollection = db.collection('test')
-	const cursor = testCollection.find()
-	cursor.toArray()
-    .then(results => {
-      console.log(results)
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+
+    
+    // ========================
+    // Middlewares
+    // ========================
+    app.set('view engine', 'ejs')
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.get('/', (req, res) => {
+      db.collection('quotes').find().toArray()
+        .then(results => {
+          res.render('index.ejs', { quotes: results })
+        })
+        .catch(/* ... */)
     })
-    .catch(error => console.error(error))
+    
+    app.post('/quotes', (req, res) => {
+      quotesCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.listen(3000, function() {
+      console.log('listening on 3000')
+    }
+    )
   })
-  .catch(error => console.error(error))
+  .catch(error => console.error('ERROR:' + error))
